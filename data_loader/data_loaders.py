e58fed3178ba307d85a06d0f6fa96c78c3d6b53c
OrionChocoPie <nogayev.nuras@gmail.com>
2021-03-15 01:16:27 +0300

    fix bugs

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,5 @@
 from base import BaseDataLoader
-from sklearn import datasets
+from sklearn.datasets import make_circles, make_moons
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
@@ -9,7 +9,7 @@ import torch
 
 class Circles(Dataset):
     def __init__(self, n_samples, shuffle, noise, factor=.8):
-        self.X, self.y = datasets.make_circles(n_samples=n_samples, shuffle=shuffle,
+        self.X, self.y = make_circles(n_samples=n_samples, shuffle=shuffle,
                                                noise=noise, factor=factor)
 
         self.X = StandardScaler().fit_transform(self.X)
@@ -23,7 +23,7 @@ class Circles(Dataset):
 
 class Moons(Dataset):
     def __init__(self, n_samples, shuffle, noise):
-        self.X, self.y = datasets.make_moons(n_samples=n_samples, shuffle=shuffle,
+        self.X, self.y = make_moons(n_samples=n_samples, shuffle=shuffle,
                                                noise=noise)
 
         self.X = StandardScaler().fit_transform(self.X)
@@ -40,17 +40,4 @@ class HomeTaskDataLoader(BaseDataLoader):
     def __init__(self, batch_size, shuffle=True, validation_split=0.0, num_workers=1, 
             training=True):
         self.dataset = Moons(5000, True, 0.2)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- model/model.py
+++ model/model.py
@@ -3,23 +3,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 
+def find_previous_layer(i, layers):
+    if i == 0:
+        return None
+    
+    for j in range(i-1, -1, -1):
+        if type(layers[j]) is int:
+            return layers[j]
+
 class HomeTaskModel(BaseModel):
     def __init__(self, layers_list):
         super(HomeTaskModel, self).__init__()
         
         layers = []
-        for layer in layers_list:
-            layers.append(torch.nn.Linear(layer[0], layer[1]))
+        for i, layer in enumerate(layers_list):
+            previous_layer = find_previous_layer(i, layers_list)
+            if previous_layer is None:
+                continue
 
-            if (layer[2] == 'sigmoid'):
-                layers.append(torch.nn.Sigmoid())
-            elif (layer[2] == 'relu'):
-                layers.append(torch.nn.ReLU())
-            elif (layer[2] == 'tanh'):
-                layers.append(torch.nn.Tanh())
+            if (layer == 'sigmoid'):
+                current_layer = nn.Sigmoid()
+            elif (layer == 'relu'):
+                current_layer = nn.ReLU()
+            elif (layer == 'tanh'):
+                current_layer = nn.Tanh()
+            else:
+                current_layer = nn.Linear(previous_layer, layer)
+
+            layers.append(current_layer)
 
         layers.append(torch.nn.Softmax())
-        self.net = torch.nn.Sequential(*layers)
+        self.net = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.net(x)
