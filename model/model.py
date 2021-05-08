e1b881b1f63fa1cc70cd7528d643bf40b5f1d7f0
Nuras Nogayev <nogayev.nuras@gmail.com>
2021-05-09 02:07:22 +0300

    Fix environment (#2)
    * update requirements.txt
    
    * format project with black
    
    * fix test
    
    * add instruction in readme

--- model/model.py
+++ model/model.py
@@ -3,29 +3,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 
+
 def find_previous_layer(i, layers):
     if i == 0:
         return None
-    
-    for j in range(i-1, -1, -1):
+
+    for j in range(i - 1, -1, -1):
         if type(layers[j]) is int:
             return layers[j]
+
 
 class HomeTaskModel(BaseModel):
     def __init__(self, layers_list):
         super(HomeTaskModel, self).__init__()
-        
+
         layers = []
         for i, layer in enumerate(layers_list):
             previous_layer = find_previous_layer(i, layers_list)
             if previous_layer is None:
                 continue
 
-            if (layer == 'sigmoid'):
+            if layer == "sigmoid":
                 current_layer = nn.Sigmoid()
-            elif (layer == 'relu'):
+            elif layer == "relu":
                 current_layer = nn.ReLU()
-            elif (layer == 'tanh'):
+            elif layer == "tanh":
                 current_layer = nn.Tanh()
             else:
                 current_layer = nn.Linear(previous_layer, layer)
